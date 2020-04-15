0577a97b6680258990ff213b90ae9cfb17815da3
Brian Chang <brianhwchang@Brians-MacBook-Pro.local>
2020-04-15 22:51:58 +0800

    working commit w/ asserts

--- Homework 1/Homework 1/main.cpp
+++ Homework 1/Homework 1/main.cpp
@@ -15,35 +15,35 @@ using namespace std;
 
 int main()
 {
-//    Set ss;
-//    ss.insert("lavash");
-//    ss.insert("roti");
-//    ss.insert("chapati");
-//    ss.insert("injera");
-//    ss.insert("roti");
-//    ss.insert("matzo");
-//    ss.insert("injera");
-//    assert(ss.size() == 5);  // duplicate "roti" and "injera" were not added
-//    string x;
-//    ss.get(0, x);
-//    assert(x == "chapati");  // "chapati" is greater than exactly 0 items in ss
-//    ss.get(4, x);
-//    assert(x == "roti");  // "roti" is greater than exactly 4 items in ss
-//    ss.get(2, x);
-//    assert(x == "lavash");  // "lavash" is greater than exactly 2 items in ss
+    Set ss;
+    ss.insert("lavash");
+    ss.insert("roti");
+    ss.insert("chapati");
+    ss.insert("injera");
+    ss.insert("roti");
+    ss.insert("matzo");
+    ss.insert("injera");
+    assert(ss.size() == 5);  // duplicate "roti" and "injera" were not added
+    string x;
+    ss.get(0, x);
+    assert(x == "chapati");  // "chapati" is greater than exactly 0 items in ss
+    ss.get(4, x);
+    assert(x == "roti");  // "roti" is greater than exactly 4 items in ss
+    ss.get(2, x);
+    assert(x == "lavash");  // "lavash" is greater than exactly 2 items in ss
     
-//    Set ss;
-//    ss.insert("dosa");
-//    assert(!ss.contains(""));
-//    ss.insert("tortilla");
-//    ss.insert("");
-//    ss.insert("focaccia");
-//    assert(ss.contains(""));
-//    ss.erase("dosa");
-//    assert(ss.size() == 3  &&  ss.contains("focaccia")  &&  ss.contains("tortilla")  &&  ss.contains(""));
-//    string v;
-//    assert(ss.get(1, v)  &&  v == "focaccia");
-//    assert(ss.get(0, v)  &&  v == "");
+    Set gg;
+    gg.insert("dosa");
+    assert(!gg.contains(""));
+    gg.insert("tortilla");
+    gg.insert("");
+    gg.insert("focaccia");
+    assert(gg.contains(""));
+    gg.erase("dosa");
+    assert(gg.size() == 3  &&  gg.contains("focaccia")  &&  gg.contains("tortilla")  &&  gg.contains(""));
+    string v;
+    assert(gg.get(1, v)  &&  v == "focaccia");
+    assert(gg.get(0, v)  &&  v == "");
     
     
     // Code for testing my swap function.
