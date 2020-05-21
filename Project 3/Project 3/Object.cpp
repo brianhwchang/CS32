c2075b7d4e795794d430a732b54fb547e5565b76
Brian Chang <brianhwchang@Brians-MacBook-Pro-2.local>
2020-05-21 13:56:57 +0800

    object class done

--- Project 3/Project 3/Object.cpp
+++ Project 3/Project 3/Object.cpp
@@ -7,3 +7,85 @@
 //
 
 #include "Object.h"
+
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//      WEAPON CLASS IMPLEMENTATIONS
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+//Regular Constructors
+
+Weapon::Weapon(string name, string action, int dexb, int dmg)
+:Object(name, action), dexBonus(dexb), dmgAmt(dmg) { }
+
+Mace::Mace()
+:Weapon("mace", "swings", 0, 2) {}
+
+ShortSword::ShortSword()
+:Weapon("short sword", "slashes", 0, 2) {}
+
+LongSword::LongSword()
+:Weapon("long sword", "swings", 2, 4) {}
+
+MagicAxe::MagicAxe()
+:Weapon("magic axe", "chops", 5, 5) {}
+
+MagicFangs::MagicFangs()
+:Weapon("magic fangs", "strikes", 3, 2) {}
+
+//Drop Constructors (Because they include positions)
+
+Weapon::Weapon(int row, int col, string name, string action, int dexb, int dmg)
+:Object(row, col, name, action), dexBonus(dexb), dmgAmt(dmg) { }
+
+MagicAxe::MagicAxe(int row, int col)
+:Weapon(row, col, "magic axe", "chops", 5, 5) {}
+
+MagicFangs::MagicFangs(int row, int col)
+:Weapon(row, col, "magic fangs", "strikes", 3, 2) {}
+
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//      SCROLL CLASS IMPLEMENTATIONS
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+//Regular Constructors
+
+Scroll::Scroll(string name, string action)
+:Object(name, action) {}
+
+HPScroll::HPScroll()
+:Scroll("scroll of enhance health", "You feel your heart beating stronger.") {}
+
+ArmorScroll::ArmorScroll()
+:Scroll("scroll of enhance armor", "Your armor glows blue.") {}
+
+StrengthScroll::StrengthScroll()
+:Scroll("scroll of strength" , "Your muscles bulge.") {}
+
+DexScroll::DexScroll()
+:Scroll("scroll of enhance dexterity", "You feel like less of a klutz.") {}
+
+TPScroll::TPScroll()
+:Scroll("scroll of teleportation", "You feel your body wrenched in space and time.") {}
+
+
+//Drop Constructors (Because they include positions)
+
+Scroll::Scroll(int row, int col, string name, string action)
+:Object(row, col, name, action) {}
+
+HPScroll::HPScroll(int row, int col)
+:Scroll(row, col, "scroll of enhance health", "You feel your heart beating stronger.") {}
+
+ArmorScroll::ArmorScroll(int row, int col)
+:Scroll(row, col, "scroll of enhance armor", "Your armor glows blue.") {}
+
+StrengthScroll::StrengthScroll(int row, int col)
+:Scroll(row, col, "scroll of strength" , "Your muscles bulge.") {}
+
+DexScroll::DexScroll(int row, int col)
+:Scroll(row, col, "scroll of enhance dexterity", "You feel like less of a klutz.") {}
+
+TPScroll::TPScroll(int row, int col)
+:Scroll(row, col, "scroll of teleportation", "You feel your body wrenched in space and time.") {}
--- Project 3/Project 3/Object.h
+++ Project 3/Project 3/Object.h
@@ -22,18 +22,23 @@ class Object
 public:
     
     //Constructors and Destructors
-    Object();
+
+    //Position Only
     Object(int row, int col) {m_row = row; m_col = col;}
+    //Name and Action Only.
+    Object(string name, string action) {m_name = name; m_action = action;}
+    //Position and Name Only.
     Object(int row, int col, string name) {m_row = row; m_col = col; m_name = name;}
-    Object(int row, int col, string name, string effect) {m_row = row; m_col = col; m_name = name; m_effect = effect;}
-    Object(string name, string effect) {m_name = name; m_effect = effect;}
+    //Postion, Name, Action.
+    Object(int row, int col, string name, string action) {m_row = row; m_col = col; m_name = name; m_action = action;}
+    
     virtual ~Object() {};
     
     //Getters
     int getRow() const {return m_row;}
     int getCol() const {return m_col;}
     string getName() const {return m_name;}
-    string getEffect() const {return m_effect;}
+    string getAction() const {return m_action;}
     
     //Setters
     void setPos(int row, int col) {m_row = row; m_col = col;}
@@ -42,7 +47,107 @@ private:
     int m_row;
     int m_col;
     string m_name;
-    string m_effect;
+    string m_action;
+};
+
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//----------------WEAPON CLASSES-----------------
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+class Weapon : public Object
+{
+public:
+    Weapon(string name, string action, int dexb, int dmg);
+    Weapon(int row, int col, string name, string action, int dexb, int dmg);
+    virtual ~Weapon() {};
+    
+    int getDex() {return dexBonus;}
+    int getDmg() {return dmgAmt;}
+    
+private:
+    int dexBonus;
+    int dmgAmt;
+};
+
+class Mace : public Weapon
+{
+    Mace();
+    ~Mace() {};
+};
+
+class ShortSword : public Weapon
+{
+    ShortSword();
+    ~ShortSword() {};
+};
+
+class LongSword : public Weapon
+{
+    LongSword();
+    ~LongSword() {};
+};
+
+class MagicAxe : public Weapon
+{
+    MagicAxe();
+    MagicAxe(int row, int col);         //Drop Constructor
+    ~MagicAxe() {};
+};
+
+class MagicFangs : public Weapon
+{
+    MagicFangs();
+    MagicFangs(int row, int col);       //Drop Constructor
+    ~MagicFangs() {};
+};
+
+
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//----------------SCROLL CLASSES-----------------
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+class Scroll : public Object
+{
+public:
+    Scroll(string name, string action);
+    Scroll(int row, int col, string name, string action);
+    virtual ~Scroll() {}
+};
+
+class HPScroll : public Scroll
+{
+    HPScroll();
+    HPScroll(int row, int col);
+    ~HPScroll() {}
+};
+
+class ArmorScroll : public Scroll
+{
+    ArmorScroll();
+    ArmorScroll(int row, int col);
+    ~ArmorScroll() {}
+};
+
+class StrengthScroll : public Scroll
+{
+    StrengthScroll();
+    StrengthScroll(int row, int col);
+    ~StrengthScroll() {}
+};
+
+class DexScroll : public Scroll
+{
+    DexScroll();
+    DexScroll(int row, int col);
+    ~DexScroll() {}
+};
+
+class TPScroll : public Scroll
+{
+    TPScroll();
+    TPScroll(int row, int col);
+    ~TPScroll() {};
 };
 
 
